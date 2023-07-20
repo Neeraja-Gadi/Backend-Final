fc55b459a75720e60920b2eb6add692a342cb802
Neeraja-Gadi <106535298+Neeraja-Gadi@users.noreply.github.com>
2023-07-20 11:14:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-require('dotenv').config({path: './.env'});
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const route = require('./src/Routes/router');
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(
-    cors({
-      origin: 'http://hiclousia.com',
-      methods: ['GET', 'POST', 'PUT', 'DELETE' , "PATCH"],
-    })
-  );
-  
-app.use('/', route);
+dotenv.config();
 
 // DATABASE CONNECTION
 mongoose
-    .connect('mongodb+srv://Neeraja:Hiclousia@123@cluster0.koj69cg.mongodb.net/?retryWrites=true&w=majority', {
+    .connect(process.env.DATABASE , {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
@@ -28,6 +20,10 @@ mongoose
         console.log('MongoDB Is Connected To Hiclousia');
     })
     .catch((err) => console.log(err));
+
+app.use( cors());
+app.use(express.json());
+app.use('/', route);
 // PORT
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
